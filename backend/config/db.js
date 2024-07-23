46a1215f61e683438b00fe9b6872a04d076e45b0
Ethan Fajnkuchen <102037640+EthanFajnkuchen@users.noreply.github.com>
2024-07-23 16:22:33 +0300

    try to fix db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI + '/testMoveo'
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
