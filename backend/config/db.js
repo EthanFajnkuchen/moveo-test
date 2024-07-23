11feed0887e5ddcb02ced986c85488f9b9aa52d1
Ethan Fajnkuchen <102037640+EthanFajnkuchen@users.noreply.github.com>
2024-07-23 15:54:54 +0300

    change to mongo uri env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const MONGO_URI = 'mongodb://127.0.0.1:27017/testMoveo'; 
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
